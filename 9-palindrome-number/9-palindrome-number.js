79bef115893d8e215ade4cca3a96ee4f825ed451
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-07-19 12:51:49 -0400

    Time: 318 ms (27.47%), Space: 50.3 MB (90.00%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,26 +4,17 @@
  */
 var isPalindrome = function (x) {
     let numArray = Array.from(String(x), Number);
+    let iterations = numArray.length;
 
-    for (let i = 0; i < 500; i++) {
+    for (let i = 0; i < iterations; i++) {
           if ((numArray.length == 1 || (numArray.length === 0))) {
                 return true;
             }
-        
-        console.log(numArray[0]);
-        console.log(numArray[numArray.length - 1]);
         if (numArray[0] != numArray[numArray.length - 1]) {
-
             return false;
-
         } else {
-            console.log(numArray);
             numArray = numArray.slice(1, -1)
-            console.log(numArray);
-       
             }
-        
-          
     }
     return true;
 };
