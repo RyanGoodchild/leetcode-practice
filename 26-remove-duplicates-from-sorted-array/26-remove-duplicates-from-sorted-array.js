0574db9b710c5d44d4d48ce5af8146c2ee9bfde5
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-07-21 14:19:30 -0400

    Time: 171 ms (22.60%), Space: 45.2 MB (17.11%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -6,9 +6,7 @@ var removeDuplicates = function(nums) {
     
     
     for (let i = 0; i < nums.length-1; i++) {
-        console.log(nums[i]);
         if (nums[i] == nums[i+1]) {
-            console.log("This one hit inside the IF " + nums[i]);
             nums.splice(i,1);
             i--;
         }
