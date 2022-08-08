6ead4003793de5269225ad6da08b97585e455b86
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-08 15:32:58 -0400

    Time: 96 ms (34.29%), Space: 41.9 MB (76.57%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -5,22 +5,12 @@
 var lengthOfLastWord = function(s) {
     
     let wordArray = s.split(" ");
-    let backPointer = wordArray.length - 1;
-    // let lastWord = wordArray[wordArray.length - 1];
-console.log(wordArray);
-    console.log(wordArray.length);
+
     
     for (let i = wordArray.length - 1; i >= 0; i--) {
-             console.log(i);
-        console.log(wordArray[i]);
         if (wordArray[i] != '') {
             return wordArray[i].length;
         }
-        
-    }
-    
-
-
-    
+    }   
     
 };
