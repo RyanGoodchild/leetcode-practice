41816fb840a8adf349084772a66ea7fc9d44536d
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-11 15:42:16 -0400

    Time: 118 ms (12.14%), Space: 42.5 MB (22.15%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    
+    
+    for (let i = 0; i < t.length; i++) {
+        
+        if (t[i] == s[0]) {
+            s = s.substring(1);
+        }
+    }
+    
+    if (s.length > 0) {
+        return false;
+    } else {
+        return true;
+    }
+    
+    
+};
