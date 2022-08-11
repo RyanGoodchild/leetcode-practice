673e7c9b84d785212e48718e6c2a9f16a9ce0c78
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-11 16:20:23 -0400

    Time: 73 ms (95.98%), Space: 44.1 MB (86.81%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -8,30 +8,17 @@ var isPalindrome = function(s) {
     if (s.length === 1) {
         return true;
     }
-    console.log(s);
-    // convert the string for use
     let drome = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
-    console.log(drome);
+
     
-    // if (drome.length === 1) {
-    //     return false;
-    // }
-    
-    if (s == " " || drome.length === 0) {
-        return true;
-    }
-  
+//     if (s == " " || drome.length === 0) {
+//         return true;
+//     }
     
      for (let i = 0; i < drome.length; i++) {
-     
          if (drome[i] !== drome[drome.length - (i + 1)]) {
-          
              return false;
-         }
-         
-         
+         }    
     }
-      console.log("HIT IN HERE")
-    return true;
-    
+    return true; 
 };
