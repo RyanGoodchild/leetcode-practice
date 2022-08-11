cbf568d9d57522616127528ace0b31ef334005ec
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-11 16:20:35 -0400

    Time: 78 ms (91.64%), Space: 44.6 MB (70.61%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,15 +5,11 @@
 var isPalindrome = function(s) {
 
     
-    if (s.length === 1) {
-        return true;
-    }
-    let drome = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
-
-    
-//     if (s == " " || drome.length === 0) {
+//     if (s.length === 1) {
 //         return true;
 //     }
+    let drome = s.replace(/[^a-z0-9]/gi,'').toLowerCase();
+
     
      for (let i = 0; i < drome.length; i++) {
          if (drome[i] !== drome[drome.length - (i + 1)]) {
