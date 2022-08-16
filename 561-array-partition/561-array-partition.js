ffbf4280eb34c9bd28ddff1b0f7736175e4027d7
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-16 12:53:26 -0400

    Time: 192 ms (35.99%), Space: 47.1 MB (91.07%) - LeetHub

--- 561-array-partition/561-array-partition.js
+++ 561-array-partition/561-array-partition.js
@@ -4,12 +4,9 @@
  */
 var arrayPairSum = function(nums) {
     
-    // sort array
-    // inner and outer pointer
-    // add every other to the total.
+  
     nums.sort(function(a,b) { return a - b; });
     let total = 0;
-    console.log(nums);
     
     for (let i = 0; i < nums.length; i+=2) {
         total = total + nums[i];
