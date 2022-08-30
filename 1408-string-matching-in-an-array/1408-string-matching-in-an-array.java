c538716cd80ba609b4a3c1a179e07b725cee7c37
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-08-30 15:48:56 -0400

    Time: 6 ms (57.65%), Space: 42.9 MB (46.62%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.java
@@ -1,20 +1,16 @@
 class Solution {
     public List<String> stringMatching(String[] words) {
         
-        
         List<String> answers = new ArrayList();
-        // cycle through all words and see if each word contains sub words
         for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                
+            for (int j = 0; j < words.length; j++) {    
                 if (j != i) {
                     if (words[i].contains(words[j]) && !answers.contains(words[j])) {
-                        answers.add(words[j]);
-                       
+                        answers.add(words[j]);   
                     }
                 }
         }
-        
     }
         return answers;
-}}
+}
+}
