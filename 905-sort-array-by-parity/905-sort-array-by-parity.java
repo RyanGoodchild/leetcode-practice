7208c8e496315dc72602b27a921ebb2dcea2440b
RyanGoodchild <93943772+RyanGoodchild@users.noreply.github.com>
2022-09-06 15:06:32 -0400

    Time: 2 ms (50.02%), Space: 48.5 MB (29.26%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.java
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -18,27 +18,6 @@ class Solution {
         }
         
         return answer;
-//         // List<Integer> evens = new ArrayList<>();
-//         int evens[] = new int[nums.length];
-//         // List<Integer> odds = new ArrayList<>();
-//         int odds[] = new int[nums.length];
-        
-//         for (int i = 0; i < nums.length; i++ ){
-//             if (nums[i] % 2 == 0) {
-//                 evens[i] = nums[i];
-//             } else {
-//                  odds[i] = nums[i];
-//             }
-//         }
-        
-        
-//         int ans[] = new int[nums.length];
-        
-//         for (int j = 0; j < nums.length; j++) {
-//             ans.add(odds[j]);
-//         }
-        
-        
-//     return ans;
+
     }
 }
